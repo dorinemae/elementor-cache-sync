794a7b4fd8600f31be32f96cb6671a31930a1fd8
Dorine Mae Biag <dm@Dorines-MBPro.local>
2025-03-29 00:18:17 +0800

    Renamed button.

--- elementor-cache-sync.php
+++ elementor-cache-sync.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Elementor Cache & Sync One-Click
  * Description: Adds a one-click button in the admin bar to clear Elementor cache and sync the library automatically.
- * Version: 3.7
+ * Version: 3.8
  * Author: dorinemae.com
  */
 
@@ -18,7 +18,7 @@ add_action('admin_bar_menu', function ($wp_admin_bar) {
 
     $wp_admin_bar->add_node([
         'id'    => 'elementor_cache_sync',
-        'title' => '🧹 Elementor',
+        'title' => 'CleanUp Elementor',
         'href'  => '/wp-admin/admin.php?page=elementor-tools&cache_sync_trigger=true',
     ]);    
 }, 100);
